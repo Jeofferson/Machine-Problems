b5984fec6787bc7bc38bf975de85232130476c7e
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-05-16 12:53:11 +0800

    revised mp001

--- MP001RemoveSubstringFromString/mp_001.py
+++ MP001RemoveSubstringFromString/mp_001.py
@@ -13,16 +13,15 @@ did_match = False
 index_where_they_matched = 0
 
 for i in range(0, input_string_length - (substring_to_remove_length - 1)):
-    if (input_string[0:substring_to_remove_length] == substring_to_remove):
+    if input_string[0:substring_to_remove_length] == substring_to_remove:
         did_match = True
         index_where_they_matched = i
         break
     input_string = input_string[1:]
         
 if not did_match:
-    print(original_input_string)
+    print("Output:", original_input_string)
 else:
     first_part = original_input_string[0:index_where_they_matched]
     last_part = original_input_string[index_where_they_matched + substring_to_remove_length:]
-    
-print("Output:", first_part + last_part)
+    print("Output:", first_part + last_part)
