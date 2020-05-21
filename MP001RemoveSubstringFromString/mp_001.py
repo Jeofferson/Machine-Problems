4f5e056eb43a3221bce063c3e4e63ead1a4b11cd
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-05-21 10:55:30 +0800

    mp1 removes ALL substrings

--- MP001RemoveSubstringFromString/mp_001.py
+++ MP001RemoveSubstringFromString/mp_001.py
@@ -1,27 +1,39 @@
-input_string = original_input_string = input("Input string: ")
+input_string = input("Input string: ")
 substring_to_remove = input("Substring to remove: ")
 
-input_string_length = 0
-for i in input_string:
-    input_string_length += 1
+output_string = ""
 
 substring_to_remove_length = 0
 for i in substring_to_remove:
     substring_to_remove_length += 1
 
-did_match = False
-index_where_they_matched = 0
+should_repeat = True
 
-for i in range(0, input_string_length - (substring_to_remove_length - 1)):
-    if input_string[0:substring_to_remove_length] == substring_to_remove:
-        did_match = True
-        index_where_they_matched = i
-        break
-    input_string = input_string[1:]
+while should_repeat:
+    should_repeat = False
+
+    original_input_string = input_string
+    input_string_length = 0
+    for i in input_string:
+        input_string_length += 1
+    
+    index_where_they_matched = 0
+
+    for i in range(0, input_string_length - (substring_to_remove_length - 1)):
+        if input_string[0:substring_to_remove_length] == substring_to_remove:
+            should_repeat = True
+            index_where_they_matched = i
+            break
         
-if not did_match:
-    print("Output:", original_input_string)
-else:
-    first_part = original_input_string[0:index_where_they_matched]
-    last_part = original_input_string[index_where_they_matched + substring_to_remove_length:]
-    print("Output:", first_part + last_part)
+        input_string = input_string[1:]
+
+    if not should_repeat:
+        input_string = original_input_string
+        break
+    else:
+        first_part = original_input_string[0:index_where_they_matched]
+        last_part = original_input_string[index_where_they_matched + substring_to_remove_length:]
+        output_string = first_part + last_part
+        input_string = output_string
+
+print("Output:", input_string)
